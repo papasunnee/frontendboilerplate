c2a41df01b966d321d4e6d92268c75718bda4e4c
Adetona Sunday <papasunnee@gmail.com>
2019-01-18 01:53:41 +0100

    done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import Link from "next/link";
+import Title from "../components/Styled/Title";
 
 export default class Index extends Component {
   state = {};
@@ -7,6 +8,7 @@ export default class Index extends Component {
   render() {
     return (
       <Fragment>
+        <Title>Sunday</Title>
         <h1>{process.env.PORT}</h1>
         <Link href="/about">
           <a className="btn btn-light">About us</a>
